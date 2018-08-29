baa2be90e21ae60aad35cb14aaf459047db22c2b
sw <siversonw@126.com>
2018-08-29 23:09:45 +0800

    first commit

---
+++ src/SayHello.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace justfun; 
+
+class SayHello {
+	
+   public static function world()
+    {
+        return 'Hello World!';
+    }
+    
+}
---
+++ test.php
@@ -0,0 +1,8 @@
+<?php
+
+require './vendor/autoload.php';
+
+use justfun\SayHello; 
+
+echo SayHello::world(); 
+
